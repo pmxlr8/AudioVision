f0f56ae8523e90163f3c0bd2f27d07ffcb6c679a
Akshat Arora <akshatarora130@gmail.com>
2024-08-18 18:18:36 +0530

    Update utils.ts

--- amd-app/apps/web/src/app/utils.ts
+++ amd-app/apps/web/src/app/utils.ts
@@ -3,11 +3,10 @@ import { Upload } from "@aws-sdk/lib-storage";
 
 const s3Client = new S3Client({
   region: "auto",
-  endpoint: "https://b1cf212831a715e8f78595eac3587c44.r2.cloudflarestorage.com",
+  endpoint: process.env.NEXT_PUBLIC_ENDPOINT || "",
   credentials: {
-    accessKeyId: "ac5d52d7380e08d116334a3babbf2ecf",
-    secretAccessKey:
-      "ad0f3785ae8c25c7f80fbae75936efd2a7ae463d3a67fb201193ae9f116022b4",
+    accessKeyId: process.env.NEXT_PUBLIC_ACESS_ID || "",
+    secretAccessKey: process.env.NEXT_PUBLIC_SECRET || "",
   },
 });
 
